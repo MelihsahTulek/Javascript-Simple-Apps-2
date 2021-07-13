a32601d2f7f725441461f465aa1d1ab5425b07e2
Melihşah <70286075+MelihsahTulek@users.noreply.github.com>
2021-07-14 01:13:02 +0300

    Update app.js

--- accordion/app.js
+++ accordion/app.js
@@ -3,7 +3,6 @@ const ul = document.querySelector(".ul");
 for (let i = 0; i < ul.children.length; i++) {
   ul.children[i].children[0].addEventListener("click", () => {
     if (ul.children[i].children[1].getAttribute("class").includes("active")) {
-      console.log("object");
       ul.children[i].children[1].classList.remove("active");
       ul.children[i].children[0].children[1].classList.remove("active");
     } else {
