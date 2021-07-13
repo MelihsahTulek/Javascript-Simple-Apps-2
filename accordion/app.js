146c8e9c5a9f51404deae9277e62d57f22a6708b
Melihşah <70286075+MelihsahTulek@users.noreply.github.com>
2021-07-14 01:12:22 +0300

    Add files via upload

---
+++ accordion/app.js
@@ -0,0 +1,18 @@
+const ul = document.querySelector(".ul");
+
+for (let i = 0; i < ul.children.length; i++) {
+  ul.children[i].children[0].addEventListener("click", () => {
+    if (ul.children[i].children[1].getAttribute("class").includes("active")) {
+      console.log("object");
+      ul.children[i].children[1].classList.remove("active");
+      ul.children[i].children[0].children[1].classList.remove("active");
+    } else {
+      for (let i = 0; i < ul.children.length; i++) {
+        ul.children[i].children[1].classList.remove("active");
+        ul.children[i].children[0].children[1].classList.remove("active");
+      }
+      ul.children[i].children[1].classList.add("active");
+      ul.children[i].children[0].children[1].classList.add("active");
+    }
+  });
+}
